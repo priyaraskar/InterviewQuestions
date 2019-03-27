3819540b047f799e13db9969061c9d0f749f965e
Priya <priyaraskar@gmail.com>
2019-03-26 18:22:42 -0700

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+﻿var reversedArray = "";
+var inputValue = "The food is hot";
+var array = [];
+array = inputValue.split(' ');
+
+jQuery.each(array, function (index, value) {
+    for (var i = value.length - 1; i >= 0; i--) { //for each character
+        reversedArray += value[i];
+    }
+    reversedArray += " ";
+});
+//$("#txtValue").value = reversedArray;
+console.log(reversedArray);
